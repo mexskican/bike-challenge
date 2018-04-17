928d3acc7f008eca6b454e3bd773f78a87c06c9f
mexskican <benoit.verkindt@gmail.com>
2018-04-17 02:19:39 +0200

    Add configuration file

--- truffle.js
+++ truffle.js
@@ -1,4 +1,10 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "4", // Rinkeby network, the public Geth PoA testnet
+      gas: 6700000
+    }
+  }
 };
